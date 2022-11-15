29242989225ee096c397536c5e6b6d3e9b92ce9a
Lorena Denhoff <66786179+LoreDenhoff@users.noreply.github.com>
2022-11-15 01:07:27 -0300

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,14 @@
+    let contras = ["Admi123", "admi1234", "Admi456", "admi456"]
+    let botton = document.querySelector("#boton")
+    
+    
+    botton.addEventListener("click", function(){
+      let contraseña = document.querySelector("#password").value
+    
+    if(contras.includes (contraseña)){
+        window.location.replace = ("./paginas/inicio.html") 
+        // Deberia redirigir a la página de inicio
+    }
+    else{
+        window.alert('Contraseña incorrecta')
+    }})
